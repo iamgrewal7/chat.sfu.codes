f5155e9e3c69fc0f7a63377fb28cf7798258dd5b
Harman Singh <iamgrewal@Harmans-MacBook-Pro.local>
2019-01-18 23:38:56 -0800

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import subscribeToTimer from './api.js';
+
 
 class App extends Component {
+
+   constructor(props) {
+  	super(props);
+  	subscribeToTimer((err, timestamp) => this.setState({ 
+    		timestamp 
+  	}));
+}
+
+
+  state  = {timestamp:'Nothing yet'};
   render() {
     return (
       <div className="App">
-          <h1>Welcome to the SFU Code Chat Room</h1>
+          <h1>{this.state.timestamp}</h1>
       </div>
     );
   }
---
+++ src/api.js
@@ -0,0 +1,9 @@
+import openSocket from 'socket.io-client';
+const socket = openSocket('http://localhost:8000');
+
+function subsribeToTimer(cb){
+    socket.on('timer',timestamp =>cb(null,timestamp));
+    socket.emit('subscribeToTimer', 1000);
+}
+
+export default subsribeToTimer;
