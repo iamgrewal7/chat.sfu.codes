14915b19cd9054a96b2193d00d68a48ea13296a4
Harman Singh <iamgrewal@Harmans-MacBook-Pro.local>
2019-01-22 16:19:11 -0800

    Server Changes

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
-const io = require('socket.io')();
+const app = require('express')()
+const server = require('http').Server(app)
+const io = require('socket.io')(server);
 
 io.on('connection', client =>{
     client.on('subscribeToTimer', (msg) =>{
@@ -9,6 +11,8 @@ io.on('connection', client =>{
     });
 });
 
-const port = 8000
-io.listen(port);
-console.log("Port 8000 active");
+server.listen(3000, (err) => {
+    if (err) throw err
+    console.log('> Ready on http://localhost:3000')
+  })
+console.log("Port 3000 active");
