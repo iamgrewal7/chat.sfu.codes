de7eb22c97eeefcc63646204a0346edd343dbb6e
Harman Singh <iamgrewal@Harmans-MacBook-Pro.local>
2019-02-02 07:04:19 -0800

    Add sockets and database

--- server.js
+++ server.js
@@ -1,17 +1,51 @@
+var AWS  = require('aws-sdk');
 const app = require('express')()
 const server = require('http').Server(app)
 const io = require('socket.io')(server);
 
-io.on('connection', client =>{
-    client.on('subscribeToTimer', (msg) =>{
-        console.log(msg);
-        setInterval(() =>{
-            client.emit('timer',new Date());
-        }, msg);
-    });
+const messages = []
+
+// AWS.config.update({
+//     region: "us-east-2",
+//     accessKeyId: "AKIAJ57U7P6OM2J2YTDA",
+//     secretAccessKey: "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"
+//   });
+// var docClient = new AWS.DynamoDB.DocumentClient();
+// var params = {
+//     TableName: 'Chats',
+//   }
+// docClient.scan(params,onScan);
+// function onScan(err, data){
+//     if (err) {
+//         console.error("Unable to scan the table. Error JSON:", JSON.stringify(err, null, 2));
+//     } else {
+//         // print all the movies
+//         console.log("Scan succeeded.");
+//         data.Items.forEach( message => {
+//           messages.push(message)
+//         });
+
+//         // continue scanning if we have more movies, because
+//         // scan can retrieve a maximum of 1MB of data
+//         if (typeof data.LastEvaluatedKey != "undefined") {
+//             console.log("Scanning for more...");
+//             params.ExclusiveStartKey = data.LastEvaluatedKey;
+//             docClient.scan(params, onScan);
+//         }
+//     }
+//   }
+
+console.log(messages.length)  
+
+io.on('connection', socket =>{
+    console.log('connected');
+    socket.on('message',data =>{
+        messages.push(data);
+        socket.broadcast.emit('message',data);
+    })
 });
 
-server.listen(3000, (err) => {
+server.listen(8000, (err) => {
     if (err) throw err
     console.log('> Ready on http://localhost:3000')
   })
--- src/App.js
+++ src/App.js
@@ -1,24 +1,117 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import AWS from 'aws-sdk';
 import './App.css';
-import subscribeToTimer from './api.js';
+import io from 'socket.io-client';
 
 
 class App extends Component {
+  state = {
+    field:'',
+    messages:[]
+  }
 
-   constructor(props) {
-  	super(props);
-  	subscribeToTimer((err, timestamp) => this.setState({ 
-    		timestamp 
-  	}));
-}
+  componentWillMount(){
+    this.socket = io.connect('http://localhost:8000');
+    this.socket.on('message',this.handleMessage);
+    AWS.config.update({
+      region: "us-east-2",
+      accessKeyId: "AKIAJ57U7P6OM2J2YTDA",
+      secretAccessKey: "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"
+    });
+    this.docClient = new AWS.DynamoDB.DocumentClient();
+    this.params = {
+      TableName: 'Chats',
+    }
+    this.docClient.scan(this.params,this.onScan);
+  }
+
+  onScan = (err, data) => {
+    if (err) {
+        console.error("Unable to scan the table. Error JSON:", JSON.stringify(err, null, 2));
+    } else {
+        // print all the movies
+        console.log("Scan succeeded.");
+        data.Items.forEach( message => {
+          const messages = {
+            id: message.id,
+            value: message.Msg,
+          }
+           this.setState(state => ({
+            field: '',
+            messages: state.messages.concat(messages)
+          }))
+        });
+
+        // continue scanning if we have more movies, because
+        // scan can retrieve a maximum of 1MB of data
+        if (typeof data.LastEvaluatedKey != "undefined") {
+            console.log("Scanning for more...");
+            this.params.ExclusiveStartKey = data.LastEvaluatedKey;
+            this.docClient.scan(this.params, this.onScan);
+        }
+    }
+  }
+
+  handleMessage = (message) => {
+    this.setState(state => ({ messages: state.messages.concat(message) }))
+  }
+
+  handleChange = event => {
+    this.setState({ field: event.target.value });
+  }
+
+  handleSubmit = event =>{
+    event.preventDefault()
+    var dt = new Date();
+    var id = dt.getTime();
+    const message = {
+      id: dt.getTime(),
+      value: this.state.field,
+    }
+
+    var params = {
+      TableName: 'Chats',
+      Item:{
+        'Msg': message.value,
+        'Id':id
+      }
+    }
+    this.docClient.put(params, (err,data) =>{
+      if (err) {
+        console.error("Unable to add item. Error JSON:", JSON.stringify(err, null, 2));
+      } else {
+        console.log("Added item:", JSON.stringify(data, null, 2));
+    }
+    })
+
+    this.socket.emit('message',message)
+
+    this.setState(state => ({
+      field: '',
+      messages: state.messages.concat(message)
+    }))
 
 
-  state  = {timestamp:'Nothing yet'};
+  }
+
+
   render() {
     return (
       <div className="App">
-          <h1>{this.state.timestamp}</h1>
+          <ul>
+            {this.state.messages.map(message =>
+              <li key={message.id}>{message.value}</li>
+            )}
+          </ul>
+          <form onSubmit={this.handleSubmit}>
+            <input
+              onChange={this.handleChange}
+              type="text"
+              placeholder="Hello world!"
+              value={this.state.field}
+            />
+            <button>Send</button>
+          </form>
       </div>
     );
   }
